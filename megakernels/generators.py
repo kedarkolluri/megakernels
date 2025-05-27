f1983a87325f3175989e7b118d9d7d7918c0a19a
Jordan Juravsky <jordan@mindcharger.com>
2025-05-27 11:28:54 -0700

    initial commit

---
+++ megakernels/generators.py
@@ -0,0 +1,168 @@
+import torch
+from torch import Tensor
+from tqdm import tqdm
+
+from megakernels.llama import LlamaForCausalLM
+from megakernels.mk import MK_Interpreter
+from megakernels.model_types import BatchState
+from megakernels.python_vm import PyVM_Interpreter
+from megakernels.scheduler import Schedule
+
+
+class Generator:
+    def generate(self, output_tokens: Tensor, prompt_len: int, ntok: int):
+        raise NotImplementedError
+
+
+class PyTorchGenerator(Generator):
+    def __init__(
+        self,
+        model: LlamaForCausalLM,
+    ):
+        self.model = model
+
+    def generate(self, output_tokens: Tensor, prompt_len: int, ntok: int):
+        bs = output_tokens.shape[0]
+        start_position_ids = torch.ones(
+            bs, 1, dtype=torch.long, device=self.model.device
+        ) * (prompt_len)
+
+        for i in tqdm(range(1, ntok)):
+            position_ids = start_position_ids + i
+            decode_inp = BatchState(
+                input_ids=output_tokens[:, i - 1 : i],
+                position_ids=position_ids,
+                seq_len=prompt_len + i + 1,
+            )
+            decode_output: BatchState = self.model(decode_inp)
+            assert decode_output.output_ids is not None
+            output_tokens[:, i] = decode_output.output_ids.squeeze(-1)
+
+
+class MK_Generator(Generator):
+    def __init__(
+        self,
+        model: LlamaForCausalLM,
+        interpreter: MK_Interpreter,
+        schedule: Schedule,
+        barrier_fill_val: int = 0,
+        skip_mk: bool = False,
+        skip_rest: bool = False,
+    ):
+        self.model = model
+        self.interpreter = interpreter
+        self.schedule = schedule
+
+        self.barrier_fill_val = barrier_fill_val
+        self.skip_mk = skip_mk
+        self.skip_rest = skip_rest
+
+        self.fill()
+
+    def fill(self):
+        self.schedule.globs.barriers.fill_(self.barrier_fill_val)
+
+    def replace_with_noops(self):
+        self.schedule.globs.instructions.zero_()
+
+    def run(self, input_ids: Tensor, pos_id: int):
+        if not self.skip_rest:
+            batch_state = BatchState(
+                input_ids=input_ids,
+            )
+
+            post_embedding: BatchState = self.model.model.embed_tokens(batch_state)
+            hiddens = post_embedding.hidden_states
+            assert hiddens is not None
+            self.schedule.globs.hidden_states[:] = hiddens.squeeze(1)
+
+        self.fill()
+        self.schedule.globs.pos_id = pos_id
+        if not self.skip_mk:
+            self.interpreter.interpret(self.schedule.globs)
+
+        if self.skip_rest:
+            return input_ids
+
+        logits = self.schedule.globs.logits
+        output_ids = torch.argmax(logits, dim=-1)
+
+        return output_ids
+
+    def generate(
+        self,
+        output_tokens: Tensor,
+        prompt_len: int,
+        ntok: int,
+        eos_token_ids: list[int] | None = None,
+        eos_token_check_interval: int | None = None,
+    ):
+        """
+        Return num tokens until stop seq, and total num tokens generated
+        """
+        for i in tqdm(range(1, ntok)):
+            input_ids = output_tokens[:, i - 1 : i]
+            output_ids = self.run(input_ids, pos_id=prompt_len + i)
+            output_tokens[:, i] = output_ids
+
+            if eos_token_check_interval is not None and (
+                i % eos_token_check_interval == 0 or i == ntok - 1
+            ):
+                assert output_tokens.shape[0] == 1, "batch size must be 1"
+
+                end = i
+                start = i - eos_token_check_interval
+                if start - eos_token_check_interval < 0:
+                    start = 0
+
+                to_cpu = output_tokens[0, start:end].cpu()
+                for j, token in enumerate(to_cpu):
+                    if token in eos_token_ids:
+                        # -1 because we didn't generate the first token
+                        return j + start, i - 1
+
+        return ntok, ntok - 1
+
+
+class PyVM_Generator(Generator):
+    def __init__(
+        self,
+        model: LlamaForCausalLM,
+        interpreter: PyVM_Interpreter,
+        schedule: Schedule,
+    ):
+        self.model = model
+        self.interpreter = interpreter
+        self.schedule = schedule
+
+        self.instructions = self.schedule.get_linear_instructions()
+
+    def run(self, input_ids: Tensor, pos_id: int):
+        batch_state = BatchState(
+            input_ids=input_ids,
+        )
+
+        post_embedding: BatchState = self.model.model.embed_tokens(batch_state)
+        hiddens = post_embedding.hidden_states
+        assert hiddens is not None
+        self.schedule.globs.hidden_states[:] = hiddens
+        self.schedule.globs.barriers.zero_()
+        self.schedule.globs.pos_id = pos_id
+
+        self.interpreter.interpret(self.schedule.globs, self.instructions)
+
+        output_hiddens = self.schedule.globs.hidden_states
+
+        post_embedding.hidden_states = output_hiddens
+
+        post_lm_head: BatchState = self.model.lm_head(post_embedding)
+
+        output_ids = post_lm_head.output_ids
+        assert output_ids is not None
+        return output_ids
+
+    def generate(self, output_tokens: Tensor, prompt_len: int, ntok: int):
+        for i in tqdm(range(1, ntok)):
+            input_ids = output_tokens[:, i - 1]
+            output_ids = self.run(input_ids, pos_id=prompt_len + i)
+            output_tokens[:, i] = output_ids
